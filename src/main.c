3264a593964ad639d9b5a26c20c6d20c6c5f588d
gpollast <gpollast@student.42.fr>
2025-10-16 16:33:01 +0200

    (fix) restore files

---
+++ src/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 10:39:45 by gpollast          #+#    #+#             */
+/*   Updated: 2025/10/16 15:53:01 by gpollast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int		*tab;
+	t_data	data;
+
+	if (ac < 5 || ac > 6)
+	{
+		printf("Error\nUsage: ./philo nb_philos time_to_die time_to_eat"
+			" time_to_sleep [nb_meals]\n");
+		return (1);
+	}
+	tab = parse_args(av);
+	if (!tab)
+		return (1);
+	init_data(&data, tab, ac - 1);
+	deploy_philos(&data);
+	return (0);
+}
